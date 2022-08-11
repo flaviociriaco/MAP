f243f485025a691bc964b29a9630a0f1a3150d69
flaviociriaco <flaviociriaco@yahoo.com>
2022-08-11 20:40:33 -0300

    Lição-map-dev-club

---
+++ map.js
@@ -0,0 +1,53 @@
+/*
+MAP
+- Cria um novo array, a partir do array percorrido (array original)
+- Cria um novo array, com a mesma quantidade de itens do array original
+- Aceita 3 parâmetros
+    - item do array
+    - index
+    - array completo
+*/   
+
+// const numbers = [1,2,3,4]
+const students = [
+     {name: "Rodolfo", age: 25},
+     {name: "Maria", age: 43},
+     {name: "João", age: 21},
+     {name: "Bruno", age: 43},
+     {name: "Carla", age: 13},
+     {name: "Ana", age: 20},
+     {name: "Julio", age: 26},
+ ]
+
+// const newArray = numbers.map((number) => { 
+//     return number *2
+// })
+
+// const double = number => number * 2
+// const sum = number => number + 10
+// // const double2 = function(number){
+// //     return number * 2
+// // }
+
+// const newArray2 = numbers.map(double).map(sum)
+
+//  console.log(newArray2)
+
+
+// const newArray2 = numbers.map((number) => { 
+//     const newNumber = number +10
+//     return newNumber
+// })
+
+// console.log(newArray2)
+
+// const newStudents = students.map((student) => { 
+//     const newStudent = {
+//         name: student.name + ' da silva',
+//         age: student.age - 5
+//     }
+//     return newStudent
+// })
+
+// console.log(students)
+// console.log(newStudents)
